3b85b1ce0c53321f408510e9b098121542d58057
yanagitani <takanori.yanagitani@gmail.com>
2026-06-11 08:42:33 +0900

    chore: wasi related shells

---
+++ wasm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+iname=./wc2.wat
+oname=./wc2.wasm
+
+wat2wasm \
+	"${iname}" \
+	-o "${oname}" \
+	--enable-relaxed-simd \
+	--enable-tail-call || exec sh -c '
+		echo unable to compile.
+		exit 1
+	'
+
+ls -l "${oname}"
